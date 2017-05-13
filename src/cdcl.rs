295d87aa39593077fdef26119a8bc59505fc686e
Lucky Lukert <lukert@mailbox.org>
2017-05-13 12:35:28 +0200

    StackElem

--- src/cdcl.rs
+++ src/cdcl.rs
@@ -1,23 +1,33 @@
-use formula;
-use clause;
-use literal;
+use formula::*;
+use clause::*;
+use literal::*;
+use std::vec::Vec;
 
 
 pub trait CdCl{
-    fn new(initialFormula: formula::FormulaInstance)->Self;
+    fn new(initialFormula: FormulaInstance)->Self;
     fn sat()->bool;
 }
 
 
 pub struct CdClInstance{
-    formula: formula::FormulaInstance
+    formula: FormulaInstance,
+    stack: Vec<StackElem>
+}
+
+pub enum StackElem{
+    Implied(SimpleLiteral),
+    Chosen(SimpleLiteral)
 }
 
 impl CdCl for CdClInstance{
-    fn new(initialFormula: formula::FormulaInstance)->Self{
-        return CdClInstance{formula: initialFormula};
+    fn new(initialFormula: FormulaInstance)->Self{
+        return CdClInstance{formula: initialFormula, stack: vec![]};
     }
     fn sat()->bool{
         return true;
     }
+
+
+
 }
